bd6441bf07573fc22f8094b2bcfb220536f2b0af
cosminb <cosminb@43471790-e71f-11de-92aa-3ddf338522b3>
2009-12-12 20:19:31 +0000

    Added NeHe tutorial 06.

---
+++ NeHe06/src/ro/brite/android/nehe06/GlApp.java
@@ -0,0 +1,35 @@
+package ro.brite.android.nehe06;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+public class GlApp extends Activity {
+	
+	GLSurfaceView surface;
+	GlRenderer renderer;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        surface = new GLSurfaceView(this);
+        renderer = new GlRenderer(this);
+        
+        surface.setRenderer(renderer);
+        setContentView(surface);
+    }
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		surface.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		surface.onResume();
+	}
+
+}
---
+++ NeHe06/src/ro/brite/android/nehe06/GlRenderer.java
@@ -0,0 +1,209 @@
+package ro.brite.android.nehe06;
+
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.opengl.GLU;
+import android.opengl.GLSurfaceView.Renderer;
+
+
+public class GlRenderer implements Renderer {
+
+	private Context context;
+	
+	public GlRenderer(Context context)
+	{
+		this.context = context;
+	}
+	
+	private static float[][] cubeVertexCoords = new float[][] {
+		new float[] { // top
+			 1, 1,-1,
+			-1, 1,-1,
+			-1, 1, 1,
+			 1, 1, 1
+		},
+		new float[] { // bottom
+			 1,-1, 1,
+			-1,-1, 1,
+			-1,-1,-1,
+			 1,-1,-1
+		},
+		new float[] { // front
+			 1, 1, 1,
+			-1, 1, 1,
+			-1,-1, 1,
+			 1,-1, 1
+		},
+		new float[] { // back
+			 1,-1,-1,
+			-1,-1,-1,
+			-1, 1,-1,
+			 1, 1,-1
+		},
+		new float[] { // left
+			-1, 1, 1,
+			-1, 1,-1,
+			-1,-1,-1,
+			-1,-1, 1
+		},
+		new float[] { // right
+			 1, 1,-1,
+			 1, 1, 1,
+			 1,-1, 1,
+			 1,-1,-1
+		},
+	};
+
+	private static float[][] cubeTextureCoords = new float[][] {
+		new float[] { // top
+			0,1,
+			0,0,
+			1,0,
+			1,1
+		},
+		new float[] { // bottom
+			1,1,
+			0,1,
+			0,0,
+			1,0
+		},
+		new float[] { // front
+			0,0,
+			1,0,
+			1,1,
+			0,1
+		},
+		new float[] { // back
+			1,0,
+			1,1,
+			0,1,
+			0,0
+		},
+		new float[] { // left
+			0,0,
+			1,0,
+			1,1,
+			0,1
+		},
+		new float[] { // right
+			1,0,
+			1,1,
+			0,1,
+			0,0
+		},
+	};
+	
+	private static FloatBuffer[] cubeVertexBfr;
+	private static FloatBuffer[] cubeTextureBfr;
+	
+	private IntBuffer texturesBuffer;
+	
+	private static float cubeRotX;
+	private static float cubeRotY;
+	private static float cubeRotZ;
+	
+	static
+	{
+		cubeVertexBfr = new FloatBuffer[6];
+		cubeTextureBfr = new FloatBuffer[6];
+		for (int i = 0; i < 6; i++)
+		{
+			cubeVertexBfr[i] = FloatBuffer.wrap(cubeVertexCoords[i]);
+			cubeTextureBfr[i] = FloatBuffer.wrap(cubeTextureCoords[i]);
+		}
+	}
+
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		gl.glShadeModel(GL10.GL_SMOOTH);
+		gl.glClearColor(0, 0, 0, 0);
+
+		gl.glClearDepthf(1.0f);
+		gl.glEnable(GL10.GL_DEPTH_TEST);
+		gl.glDepthFunc(GL10.GL_LEQUAL);
+		
+		gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);
+		
+		// create texture
+		gl.glEnable(GL10.GL_TEXTURE_2D);
+		texturesBuffer = IntBuffer.allocate(1);
+		gl.glGenTextures(1, texturesBuffer);
+		gl.glBindTexture(GL10.GL_TEXTURE_2D, texturesBuffer.get(0));
+		
+		// convert bitmap to texture
+		Bitmap texture = BitmapFactory.decodeResource(context.getResources(), R.drawable.nehe);
+		int texWidth = texture.getWidth();
+		int texHeight = texture.getHeight();
+		int[] pixels = new int[texWidth * texHeight];
+		texture.getPixels(pixels, 0, texWidth, 0, 0, texWidth, texHeight);
+		byte[] pixelBytes = new byte[3 * texWidth * texHeight];
+		for (int x = 0; x < texWidth; x++)
+			for (int y = 0; y < texHeight; y++)
+			{
+				// flip the image
+				int srcIdx = x * texWidth + y;
+				int dstIdx = x * texWidth + (texHeight - y - 1);
+				pixelBytes[3 * dstIdx + 2] = (byte) ((pixels[srcIdx] & 0x0000ff) >>  0);
+				pixelBytes[3 * dstIdx + 1] = (byte) ((pixels[srcIdx] & 0x00ff00) >>  8);
+				pixelBytes[3 * dstIdx + 0] = (byte) ((pixels[srcIdx] & 0xff0000) >> 16);
+			}
+		ByteBuffer pixelBuffer = ByteBuffer.wrap(pixelBytes);
+		
+		// setup texture parameters and build the texture
+		gl.glTexParameterx(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
+		gl.glTexParameterx(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_LINEAR);
+		gl.glTexImage2D(GL10.GL_TEXTURE_2D, 0, GL10.GL_RGB, texWidth, texHeight, 0, GL10.GL_RGB, GL10.GL_UNSIGNED_BYTE, pixelBuffer);
+	}
+
+	@Override
+	public void onDrawFrame(GL10 gl) {
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+		gl.glMatrixMode(GL10.GL_MODELVIEW);
+		gl.glLoadIdentity();
+		
+		// draw cube
+		
+		gl.glTranslatef(0, 0, -6);
+		gl.glRotatef(cubeRotX, 1, 0, 0);
+		gl.glRotatef(cubeRotY, 0, 1, 0);
+		gl.glRotatef(cubeRotZ, 0, 0, 1);
+		
+		gl.glBindTexture(GL10.GL_TEXTURE_2D, texturesBuffer.get(0));
+		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+		gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+		for (int i = 0; i < 6; i++) // draw each face
+		{
+			gl.glVertexPointer(3, GL10.GL_FLOAT, 0, cubeVertexBfr[i]);
+			gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, cubeTextureBfr[i]);
+			gl.glDrawArrays(GL10.GL_TRIANGLE_FAN, 0, 4);
+		}
+		gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+		gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+		
+		// update rotations
+		cubeRotX += 1.2f;
+		cubeRotY += 0.8f;
+		cubeRotZ += 0.6f;
+	}
+
+	@Override
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		// avoid division by zero
+		if (height == 0) height = 1;
+		// draw on the entire screen
+		gl.glViewport(0, 0, width, height);
+		// setup projection matrix
+		gl.glMatrixMode(GL10.GL_PROJECTION);
+		gl.glLoadIdentity();
+		GLU.gluPerspective(gl, 45.0f, (float)width / (float)height, 1.0f, 100.0f);
+	}
+
+}
