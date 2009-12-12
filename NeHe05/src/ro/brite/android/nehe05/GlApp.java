34697d7bb99d064a10f29223cc0a7435b420de6d
cosminb <cosminb@43471790-e71f-11de-92aa-3ddf338522b3>
2009-12-12 20:13:02 +0000

    Added NeHe tutorial 05.

---
+++ NeHe05/src/ro/brite/android/nehe05/GlApp.java
@@ -0,0 +1,34 @@
+package ro.brite.android.nehe05;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+public class GlApp extends Activity {
+	
+	GLSurfaceView surface;
+	GlRenderer renderer;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        surface = new GLSurfaceView(this);
+        renderer = new GlRenderer();
+        surface.setRenderer(renderer);
+        setContentView(surface);
+    }
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		surface.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		surface.onResume();
+	}
+
+}
