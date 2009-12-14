a3e64ea649e0f5f6119e6bf659bc265d62e38c53
cosminb <cosminb@43471790-e71f-11de-92aa-3ddf338522b3>
2009-12-14 22:28:30 +0000

    Added fullscreen capability for all tutorials. Use double-tap to toggle fullscreen.

--- NeHe16/src/ro/brite/android/nehe16/GlApp.java
+++ NeHe16/src/ro/brite/android/nehe16/GlApp.java
@@ -1,23 +1,38 @@
 package ro.brite.android.nehe16;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.Window;
+import android.view.WindowManager;
 
 
 public class GlApp extends Activity {
 	
-	GLSurfaceView surface;
-	GlRenderer renderer;
+	private GLSurfaceView surface;
+	private GlRenderer renderer;
+	
+	private GestureDetector gestureDetector;
+	private static boolean fullscreen;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
+        if (fullscreen) {
+        	requestWindowFeature(Window.FEATURE_NO_TITLE);  
+        	getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,   
+        	WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+        
+        gestureDetector = new GestureDetector(this, new GlAppGestureListener(this));
+        
         surface = new GLSurfaceView(this);
         renderer = new GlRenderer(this);
-        
         surface.setRenderer(renderer);
         setContentView(surface);
     }
@@ -66,4 +81,36 @@ public class GlApp extends Activity {
 		return super.onKeyDown(keyCode, event);
 	}
 
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (gestureDetector.onTouchEvent(event)) {
+			return true;
+		}
+		return super.onTouchEvent(event);
+	}
+
+	private class GlAppGestureListener extends GestureDetector.SimpleOnGestureListener
+    {
+    	private GlApp glApp;
+    	
+    	public GlAppGestureListener(GlApp glApp) {
+    		this.glApp = glApp;
+    	}
+
+		@Override
+		public boolean onDoubleTap(MotionEvent e) {
+			// toggle fullscreen flag
+			GlApp.fullscreen = !GlApp.fullscreen;
+			
+			// start a new one
+			Intent intent = new Intent(glApp, GlApp.class);
+			startActivity(intent);
+
+			// close current activity
+			glApp.finish();
+			
+			return true;
+		}
+    }
+	
 }
